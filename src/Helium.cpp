e1a3246127aba26e36cf8eedbf80ea2a27e92b14
Marc Nijdam <marc@helium.com>
2017-07-07 21:20:23 -0700

    Add poll_data to Channel

--- src/Helium.cpp
+++ src/Helium.cpp
@@ -145,13 +145,12 @@ Channel::begin(const char * name, int8_t * result)
     _channel_id     = -1;
     if (helium_status_OK == status)
     {
-        status = poll(token, &_channel_id, HELIUM_POLL_RETRIES_5S);
+        status = poll_result(token, &_channel_id, HELIUM_POLL_RETRIES_5S);
     }
 
     if (result)
     {
-        *result =
-            status == helium_status_OK && _channel_id > 0 ? 0 : _channel_id;
+        *result = status == helium_status_OK && _channel_id > 0 ? 0 : _channel_id;
     }
 
     return status;
@@ -171,13 +170,28 @@ Channel::send(void const * data, size_t len, int8_t * result)
     int      status = send(data, len, &token);
     if (helium_status_OK == status)
     {
-        status = poll(token, result, HELIUM_POLL_RETRIES_5S);
+        status = poll_result(token, result, HELIUM_POLL_RETRIES_5S);
     }
     return status;
 }
 
 int
-Channel::poll(uint16_t token, int8_t * result, uint32_t retries)
+Channel::poll_result(uint16_t token, int8_t * result, uint32_t retries)
 {
     return helium_channel_poll_result(&_helium->_ctx, token, result, retries);
+}
+
+int
+Channel::poll_data(uint16_t channel_id,
+                   void *   data,
+                   size_t   len,
+                   size_t * used,
+                   uint32_t retries)
+{
+    return helium_channel_poll_data(&_helium->_ctx,
+                                    channel_id,
+                                    data,
+                                    len,
+                                    used,
+                                    retries);
 }
