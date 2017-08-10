2f575c8bc59613b26ec92d128ec940cd9baf306d
Marc Nijdam <marc@helium.com>
2017-08-10 17:47:01 -0500

    Fix arduino IDE not liking functions that aren't pre-defined

--- examples/Downlink/Downlink.ino
+++ examples/Downlink/Downlink.ino
@@ -15,44 +15,6 @@ Helium  helium(&atom_serial);
 Channel channel(&helium);
 
 
-void
-setup()
-{
-    Serial.begin(9600);
-    Serial.println("Starting");
-
-    // Begin communication with the Helium Atom
-    // The baud rate differs per supported board
-    // and is configured in Board.h
-    helium.begin(HELIUM_BAUD_RATE);
-
-    // Connect the Atom to the Helium Network
-    connect();
-    // and ensure the channel is created
-    channel_create(CHANNEL_NAME);
-}
-
-
-void
-loop()
-{
-    // Create a buffer of the maximum Helium network packet size
-    char   data[HELIUM_MAX_DATA_SIZE];
-    size_t data_used;
-
-    // Call the poll utility function below which pings the network to
-    // ask for data.
-    channel_poll(data, HELIUM_MAX_DATA_SIZE, &data_used);
-    // Convert to a printable string
-    data[data_used] = '\0';
-    if (data_used > 0)
-    {
-        Serial.print("Data - ");
-        Serial.println(data);
-    }
-}
-
-
 int
 report_status(int status, int result = 0)
 {
@@ -119,4 +81,42 @@ channel_poll(void * data, size_t len, size_t * used)
         // Poll the channel for some data for some time
         status = channel.poll_data(data, len, used);
     } while (report_status(status) != helium_status_OK);
+}
+
+
+void
+setup()
+{
+    Serial.begin(9600);
+    Serial.println("Starting");
+
+    // Begin communication with the Helium Atom
+    // The baud rate differs per supported board
+    // and is configured in Board.h
+    helium.begin(HELIUM_BAUD_RATE);
+
+    // Connect the Atom to the Helium Network
+    connect();
+    // and ensure the channel is created
+    channel_create(CHANNEL_NAME);
+}
+
+
+void
+loop()
+{
+    // Create a buffer of the maximum Helium network packet size
+    char   data[HELIUM_MAX_DATA_SIZE];
+    size_t data_used;
+
+    // Call the poll utility function below which pings the network to
+    // ask for data.
+    channel_poll(data, HELIUM_MAX_DATA_SIZE, &data_used);
+    // Convert to a printable string
+    data[data_used] = '\0';
+    if (data_used > 0)
+    {
+        Serial.print("Data - ");
+        Serial.println(data);
+    }
 }
